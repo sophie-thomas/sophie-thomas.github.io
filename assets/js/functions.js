0364e83eb90e7d1ada1aae0a2dd24ba22a30fcdf
Sophie Thomas <sophie-thomas@outlook.com>
2019-12-15 00:56:42 +0000

    New Site Design - 2019

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,26 @@
 // JavaScript Document
 
-//type text
+(function() {
+  setRandomPhoto();
+
+  setInterval(() => {
+    setRandomPhoto();
+  }, 2500);
+})();
+
+// RANDOM PHOTO
+
+function setRandomPhoto() {
+  let num = Math.floor(Math.random() * 6) + 1;
+  document.getElementById (
+    "profileImg"
+  ).src = `assets/images/sophie${num}.jpg`;
+}
+
+// ANIMATED TEXT
 $(function(){
     $(".element").typed({
-	    strings: ["^1000thinking^1000", "^1000creating^1000", "^1000designing^1000", "^1000prototyping^1000", "^1000designing^1000"],
+	    strings: ["^2000designing and prototyping websites^2000", "^2000coding websites using HTML, CSS and JavaScript^2000", "^2000filming and editing videos^2000"],
 	    // typing speed
         typeSpeed: 0,
         // loop
