7b5e1c800b891410305f255f77a830f9c7d5005a
Sophie Thomas <sophie-thomas@outlook.com>
2026-01-18 16:41:17 +0000

    V3
    Updates home page content, project case studies and adds testimonials. Also improves the site's navigation.

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,41 +1,56 @@
 document.addEventListener('DOMContentLoaded', function () {
 // Navigation
 //-- Navigation Dropdown Toggle
-  // Select the dropdown toggle button and dropdown content
+  // Selects the dropdown toggle button and navigation links
   const dropdownToggle = document.querySelector('.dropdown-toggle');
-  const dropdownContent = document.querySelector('.dropdown-content');
+  const dropdownLinks = document.querySelector('.nav-links');
 
-  // Function to toggle the visibility of the dropdown content by checking if it is currently expanded
+  // Toggles the visibility of the navigation links based on the aria-expanded attribute
   function toggleDropdown() {
-    const isExpanded = dropdownToggle.getAttribute('aria-expanded') === 'true';
-    dropdownContent.style.display = isExpanded ? 'none' : 'grid';
-    dropdownToggle.setAttribute('aria-expanded', !isExpanded);
+    const toggleExpanded = dropdownToggle.getAttribute('aria-expanded') === 'true';
+    dropdownLinks.style.display = toggleExpanded ? 'none' : 'grid';
+    dropdownToggle.setAttribute('aria-expanded', !toggleExpanded);
   }
 
-  // Event listener for click on the dropdown toggle button
+  // Adds an click event listener on the dropdown toggle button
   dropdownToggle.addEventListener('click', toggleDropdown);
 
-  // Event listener for window resize to set the visability of the dropdown toggle
+  // Sets the visability of the dropdown toggle based on the window resize event listener
   window.addEventListener('resize', function () {
-    if (window.innerWidth >= 991) {
-      dropdownContent.style.display = 'block';
+    if (window.innerWidth >= 767) {
+      dropdownLinks.style.display = 'block';
       dropdownToggle.setAttribute('aria-expanded', true);
     } else {
-      dropdownContent.style.display = 'none';
+      dropdownLinks.style.display = 'none';
       dropdownToggle.setAttribute('aria-expanded', false);
     }
   });
 
-  // Call the resize event listener once to initially set the state based on the window width
+  // Calls the resize event listener once to initially set the state based on the window width
   window.dispatchEvent(new Event('resize'));
+  
+//-- Dropdown Toggle Icon
+// Selects the Dropdown Toggle Button
+const toggleButton = document.querySelector('.dropdown-toggle');
+
+if (toggleButton) {
+  toggleButton.addEventListener('click', function () {
+    // - Finds the icon element
+    const dropdownToggleIcon = this.querySelector('.toggle-icon');
+
+    // - Changes the icon based on the aria-expanded attribute
+    const toggleExpanded = this.getAttribute('aria-expanded') === 'true';
+    dropdownToggleIcon.textContent = toggleExpanded ? '\u2715' : '\u2630'; // ✕ (U+2715) AND ☰ (U+2630)
+  });
+}
 
 //-- Navigation Active Link
   // Gets the current URL path and navigation items
   const path = window.location.pathname.split('/').pop();
-  const navItems = document.querySelectorAll('.dropdown-content a');
+  const navLinks = document.querySelectorAll('.nav-links a');
 
   // Loops through each nav item and checks if the link matches the current path
-  navItems.forEach(link => {
+  navLinks.forEach(link => {
     const linkPath = link.getAttribute('href').split('/').pop();
     
     // -- Adds .active if path is not empty and matches the link path
